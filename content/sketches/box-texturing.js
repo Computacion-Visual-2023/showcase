ba1a8a0e4dc92c842b949f727b53ef21a3608f10
Marcos <>
2023-06-16 22:32:17 -0500

    Procedural Texturing shapes with shaders.

---
+++ content/sketches/box-texturing.js
@@ -0,0 +1,34 @@
+let pg;
+let dotsShader;
+
+function preload() {
+  // shader adapted from The Book of Shaders: 
+  //https://thebookofshaders.com/edit.php#09/marching_dots.frag
+  dotsShader = readShader('/showcase/sketches/shaders/dots.frag');
+}
+
+function setup() {
+  //P5 Library used from:
+  //https://github.com/VisualComputing/p5.treegl/blob/main/p5.treegl.js
+  // Texturing adapted taking partial code from:
+  //https://visualcomputing.github.io/docs/shaders/procedural_texturing/ 
+  //truchet_tiles.js
+  createCanvas(400, 400, WEBGL);
+  pg = createGraphics(400, 400, WEBGL);
+  textureMode(NORMAL);
+  noStroke();
+  pg.noStroke();
+  pg.textureMode(NORMAL);
+  pg.shader(dotsShader);
+  pg.emitResolution(dotsShader);
+  pg.quad(-1, -1, 1, -1, 1, 1, -1, 1);
+  texture(pg);
+}
+
+function draw() {
+  background(33);
+  orbitControl();
+  box(200, 200);
+
+}
+
