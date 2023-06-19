08fee000a0a750934da1b4a963b571be0471d84d
marcos4512 <marcos4512@users.noreply.github.com>
2023-06-19 21:29:33 +0000

    deploy: a832a42034f40c7525dd53984b2ad20d3d75156c

--- sketches/shaders/box-texturing.js
+++ sketches/shaders/box-texturing.js
@@ -10,25 +10,29 @@ function preload() {
 function setup() {
   //P5 Library used from:
   //https://github.com/VisualComputing/p5.treegl/blob/main/p5.treegl.js
-  // Texturing adapted taking partial code from:
+  //Texturing adapted taking partial code from:
   //https://visualcomputing.github.io/docs/shaders/procedural_texturing/ 
   //truchet_tiles.js
-  createCanvas(400, 400, WEBGL);
+  //...and example from P5 Reference:
+  //https://p5js.org/es/examples/3d-shader-as-a-texture.html
+    createCanvas(400, 400, WEBGL);
   pg = createGraphics(400, 400, WEBGL);
   textureMode(NORMAL);
   noStroke();
   pg.noStroke();
   pg.textureMode(NORMAL);
-  pg.shader(dotsShader);
-  pg.emitResolution(dotsShader);
-  pg.quad(-1, -1, 1, -1, 1, 1, -1, 1);
-  texture(pg);
 }
 
 function draw() {
   background(33);
   orbitControl();
+  pg.shader(dotsShader);
+  dotsShader.setUniform("u_resolution", [width, height]);
+  dotsShader.setUniform("u_time", millis() / 1000.0);
+  dotsShader.setUniform("u_mouse", [mouseX, map(mouseY, 0, height, height, 0)]);
+  //pg.emitResolution(dotsShader);
+  pg.quad(-1, -1, 1, -1, 1, 1, -1, 1);
+  texture(pg);
   box(200, 200);
-
 }
 
